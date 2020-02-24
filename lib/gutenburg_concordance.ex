835df581f66a06af54bb549a08e6892b624413f8
Zachary Reiss <zachary.reiss123@gmail.com>
2020-02-24 12:25:56 -0700

    Get response from site

--- lib/gutenburg_concordance.ex
+++ lib/gutenburg_concordance.ex
@@ -1,3 +1,13 @@
 defmodule GutenburgConcordance do
-  
+
+  def init do
+  end
+
+  def get_all_books() do
+    HTTPoison.start
+    #url = "http://www.gutenberg.org/cache/epub/#{start_number}/pg#{start_number}.txt"
+    response = HTTPoison.get("http://www.gutenberg.org/cache/epub/41807/pg41807.txt")
+    
+
+  end
 end
